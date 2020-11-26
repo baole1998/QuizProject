c452804300c066f979004454493613ec647c617f
truongthieuhuyen <nguyentientruong97@gmail.com>
2020-11-26 16:38:03 +0700

    alo

--- Menu/ReadFileTest.java
+++ Menu/ReadFileTest.java
@@ -10,9 +10,35 @@ import java.util.*;
  * đọc file
  */
 public class ReadFileTest {
-    public static List<String> readFileTest(String Test__Basic) throws FileNotFoundException{
-        return null;
+    public static List<String> readFileTest(String fileName) {
+
+        List<String> lines = Collections.emptyList();
+        try
+        {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return lines;
     }
+    public static void readFileJava(){
+        List l = readFileTest("src/Quiz_project/Menu/TestJava__Basic.txt");
+        Iterator<String> itr = l.iterator();
+        while (itr.hasNext())
+            System.out.println(itr.next());
+    }
+    public static void readFileHTML(){
+        List l = readFileTest("src/Quiz_project/Menu/TestHTML__Basic.txt");
+        Iterator<String> itr = l.iterator();
+        while (itr.hasNext())
+            System.out.println(itr.next());
+    }
+//    private static List readFileJava(String s) {
+//        return null;
+//    }
+
 //    public static void readFileTest(){
 //        try {
 //            FileReader fr = new FileReader("src/Quiz_project/Menu/TestJava__Basic.txt");
