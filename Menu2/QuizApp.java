ae9c97cfab67e13dba6298630f0a2d9d2671e89b
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-10-04 18:30:46 +0700

    features

---
+++ Menu2/QuizApp.java
@@ -0,0 +1,12 @@
+package Quiz_project.Menu2;
+
+import java.util.Scanner;
+
+public class QuizApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Menu.menu();
+        System.out.println("Bạn muốn làm gì tiếp----");
+        Menu.menu();
+    }
+}
