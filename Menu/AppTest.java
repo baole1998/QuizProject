dc588af571f18c1a2e7c371a9bbcd238ad8fcba1
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-10-15 18:50:06 +0700

    features

--- Menu/AppTest.java
+++ Menu/AppTest.java
@@ -1,12 +1,14 @@
 package Quiz_project.Menu;
 
+import Quiz_project.Menu2.Menu;
+
 import java.util.Scanner;
 
 public class AppTest {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
         Menu.menu();
-        System.out.println("Bạn muốn làm gì tiếp-----");
+        System.out.println("Bạn muốn làm gì tiếp -----");
         Menu.menu();
     }
 }
